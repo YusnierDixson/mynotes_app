63e8510c2868f404b2737cd091ec302c31338b97
Dixson <yusnierdixson@gmail.com>
2020-04-19 18:33:50 +0200

    Toto el CRUD con MSG Flash

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ const path=require('path');
 const exhbs=require('express-handlebars');
 const methodOverride=require('method-override');
 const session=require('express-session');
-
+const Handlebars = require('handlebars');
+const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access');
+const flash=require('connect-flash');
 
 //Initializations
 const app=express();
@@ -12,10 +14,21 @@ require('./database');
 //Settings
 app.set('port',process.env.PORT||3000);
 app.set('views',path.join(__dirname,'views'));//Esto es porque tenemos todo en la carpeta src
+
+const routes = require('./routes');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+// When connecting Handlebars to the Express app...
+
 app.engine('.hbs', exhbs({
 defaultLayout:'main',
 layoutsDir:path.join(app.get('views'),'layouts'),
 partialsDir:path.join(app.get('views'),'partials'),//Reutilizar partes de formularios
+// ...implement newly added insecure prototype access
+handlebars: allowInsecurePrototypeAccess(Handlebars),
 extname:'.hbs'
 }));
 app.set('view engine','.hbs');
@@ -28,8 +41,15 @@ app.use(session({
     resave: true,
     saveUninitialized:true
 }))
+app.use(flash());
 //Globals Variables
+app.use((req,res,next)=>{
+    res.locals.success_msg=req.flash('success_msg');
+    res.locals.error_msg=req.flash('error_msg');
 
+
+    next();
+})
 //Routes
 app.use(require('./routes/index'));
 app.use(require('./routes/notes'));
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,11 @@ const express=require('express');
 const router=express.Router();
 
 router.get('/',(req,res)=>{
-    res.send('Index Page');
+    res.render('index');
 });
 
 router.get('/about',(req,res)=>{
-    res.send('About Page');
+    res.render('about');
 });
 
 module.exports=router;
